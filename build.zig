174babff71010da2bade6a8a86dc00b4d3a9f0eb
Daniel Fortes <daniel.mbfm@gmail.com>
2022-02-05 18:55:58 -0300

    Added pkg.zig

--- build.zig
+++ build.zig
@@ -1,81 +1,12 @@
 const std = @import("std");
 const Builder = std.build.Builder;
+const Pkg = @import("./pkg.zig").Pkg(".");
 
-const freetypeSourcesBase: []const []const u8 = &.{
-    "src/autofit/autofit.c",
-    "src/base/ftbase.c",
-    "src/base/ftbbox.c",
-    "src/base/ftbdf.c",
-    "src/base/ftbitmap.c",
-    "src/base/ftcid.c",
-    "src/base/ftfstype.c",
-    "src/base/ftgasp.c",
-    "src/base/ftglyph.c",
-    "src/base/ftgxval.c",
-    "src/base/ftinit.c",
-    "src/base/ftmm.c",
-    "src/base/ftotval.c",
-    "src/base/ftpatent.c",
-    "src/base/ftpfr.c",
-    "src/base/ftstroke.c",
-    "src/base/ftsynth.c",
-    "src/base/fttype1.c",
-    "src/base/ftwinfnt.c",
-    "src/bdf/bdf.c",
-    "src/bzip2/ftbzip2.c",
-    "src/cache/ftcache.c",
-    "src/cff/cff.c",
-    "src/cid/type1cid.c",
-    "src/gzip/ftgzip.c",
-    "src/lzw/ftlzw.c",
-    "src/pcf/pcf.c",
-    "src/pfr/pfr.c",
-    "src/psaux/psaux.c",
-    "src/pshinter/pshinter.c",
-    "src/psnames/psnames.c",
-    "src/raster/raster.c",
-    "src/sdf/sdf.c",
-    "src/sfnt/sfnt.c",
-    "src/smooth/smooth.c",
-    "src/svg/svg.c",
-    "src/truetype/truetype.c",
-    "src/type1/type1.c",
-    "src/type42/type42.c",
-    "src/winfonts/winfnt.c",
-};
-
-const freetypeFlags = [_][]const u8{
-    "-I./include/",
-    "-DFT2_BUILD_LIBRARY",
-};
-
-pub fn createLibrary(b: *Builder) *std.build.LibExeObjStep {
+pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const freetype = b.addStaticLibrary("freetype", null);
-    freetype.setTarget(target);
-    freetype.setBuildMode(mode);
-    freetype.linkLibC();
-    freetype.addCSourceFiles(freetypeSourcesBase, &freetypeFlags);
-
-    if (target.isWindows()) {
-        freetype.addCSourceFile("builds/windows/ftsystem.c", &freetypeFlags);
-    } else if (target.isLinux()) {
-        freetype.addCSourceFile("builds/unix/ftsystem.c", &freetypeFlags);
-    } else {
-        freetype.addCSourceFile("src/base/ftsystem.c", &freetypeFlags);
-    }
-
-    if (!target.isWindows()) {
-        freetype.addCSourceFile("src/base/ftdebug.c", &freetypeFlags);
-    }
-
-    return freetype;
-}
-
-pub fn build(b: *Builder) void {
-    var freetype = createLibrary(b);
+    var freetype = Pkg.library(b, target, mode);
 
     freetype.install();
 }
---
+++ pkg.zig
@@ -0,0 +1,76 @@
+const std = @import("std");
+const Builder = std.build.Builder;
+
+pub fn Pkg(path: []const u8) type {
+    return struct {
+        const freetypeSourcesBase: []const []const u8 = &.{
+            path ++ "/src/autofit/autofit.c",
+            path ++ "/src/base/ftbase.c",
+            path ++ "/src/base/ftbbox.c",
+            path ++ "/src/base/ftbdf.c",
+            path ++ "/src/base/ftbitmap.c",
+            path ++ "/src/base/ftcid.c",
+            path ++ "/src/base/ftfstype.c",
+            path ++ "/src/base/ftgasp.c",
+            path ++ "/src/base/ftglyph.c",
+            path ++ "/src/base/ftgxval.c",
+            path ++ "/src/base/ftinit.c",
+            path ++ "/src/base/ftmm.c",
+            path ++ "/src/base/ftotval.c",
+            path ++ "/src/base/ftpatent.c",
+            path ++ "/src/base/ftpfr.c",
+            path ++ "/src/base/ftstroke.c",
+            path ++ "/src/base/ftsynth.c",
+            path ++ "/src/base/fttype1.c",
+            path ++ "/src/base/ftwinfnt.c",
+            path ++ "/src/bdf/bdf.c",
+            path ++ "/src/bzip2/ftbzip2.c",
+            path ++ "/src/cache/ftcache.c",
+            path ++ "/src/cff/cff.c",
+            path ++ "/src/cid/type1cid.c",
+            path ++ "/src/gzip/ftgzip.c",
+            path ++ "/src/lzw/ftlzw.c",
+            path ++ "/src/pcf/pcf.c",
+            path ++ "/src/pfr/pfr.c",
+            path ++ "/src/psaux/psaux.c",
+            path ++ "/src/pshinter/pshinter.c",
+            path ++ "/src/psnames/psnames.c",
+            path ++ "/src/raster/raster.c",
+            path ++ "/src/sdf/sdf.c",
+            path ++ "/src/sfnt/sfnt.c",
+            path ++ "/src/smooth/smooth.c",
+            path ++ "/src/svg/svg.c",
+            path ++ "/src/truetype/truetype.c",
+            path ++ "/src/type1/type1.c",
+            path ++ "/src/type42/type42.c",
+            path ++ "/src/winfonts/winfnt.c",
+        };
+
+        const freetypeFlags = [_][]const u8{
+            "-I" ++ path ++ "/include/",
+            "-DFT2_BUILD_LIBRARY",
+        };
+
+        pub fn library(b: *Builder, target: std.zig.CrossTarget, mode: std.builtin.Mode) *std.build.LibExeObjStep {
+            const freetype = b.addStaticLibrary("freetype", null);
+            freetype.setTarget(target);
+            freetype.setBuildMode(mode);
+            freetype.linkLibC();
+            freetype.addCSourceFiles(freetypeSourcesBase, &freetypeFlags);
+
+            if (target.isWindows()) {
+                freetype.addCSourceFile(path ++ "/builds/windows/ftsystem.c", &freetypeFlags);
+            } else if (target.isLinux()) {
+                freetype.addCSourceFile(path ++ "/builds/unix/ftsystem.c", &freetypeFlags);
+            } else {
+                freetype.addCSourceFile(path ++ "/src/base/ftsystem.c", &freetypeFlags);
+            }
+
+            if (!target.isWindows()) {
+                freetype.addCSourceFile(path ++ "/src/base/ftdebug.c", &freetypeFlags);
+            }
+
+            return freetype;
+        }
+    };
+}
